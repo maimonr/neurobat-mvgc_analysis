5b0d3baa0d60c5f660c838d15e3a9e67aea77e5b
Unknown <maimon.rose@gmail.com>
2020-02-07 16:29:02 -0800

    Auto stash before merge of "master" and "origin/master"

--- plot_fc_operant_gc.m
+++ plot_fc_operant_gc.m
@@ -6,22 +6,22 @@ colors = {'r','k'};
 nBat = 4;
 [operant_caller_idxs,operant_listener_idxs,operant_caller_bat_num,operant_listener_bat_num] = deal(zeros(1,length(FF_operant_call)));
 
-for exp_k = 1:length(FF_operant_call)
-    x = cellfun(@(x) x.used_bat_nums,trialInfo_operant{exp_k},'un',0);
-    x = [x{:}];
-    batNums = str2double(trialInfo_operant{exp_k}{1}.batNums);
-    
-    operant_caller_bat_num(exp_k) = mode(x);
-    operant_listener_bat_num(exp_k) = setdiff(batNums,operant_caller_bat_num(exp_k));
-    operant_caller_idxs(exp_k) = find(batNums == operant_caller_bat_num(exp_k));
-    operant_listener_idxs(exp_k) = setdiff(1:length(batNums),operant_caller_idxs(exp_k));
-end
+% for exp_k = 1:length(FF_operant_call)
+%     x = cellfun(@(x) x.used_bat_nums,trialInfo_operant{exp_k},'un',0);
+%     x = [x{:}];
+%     batNums = str2double(trialInfo_operant{exp_k}{1}.batNums);
+%     
+%     operant_caller_bat_num(exp_k) = mode(x);
+%     operant_listener_bat_num(exp_k) = setdiff(batNums,operant_caller_bat_num(exp_k));
+%     operant_caller_idxs(exp_k) = find(batNums == operant_caller_bat_num(exp_k));
+%     operant_listener_idxs(exp_k) = setdiff(1:length(batNums),operant_caller_idxs(exp_k));
+% end
 % 
 % for operant_idx = {operant_listener_idxs,operant_caller_idxs}
 %     X = cellfun(@(x,idx) reshape(x(:,idx,:),[size(x,1) size(x,3)]),FF_operant_call,num2cell(operant_idx{1}),'un',0);
 %     X = cat(1,X{:});
 %     mu = nanmean(X(:,operant_t<baselineT,:),'all');
-%     sigma = nanstd(X(:,operant_t<baselineT,:),[],'all');
+%     sigma = 1; %nanstd(X(:,operant_t<baselineT,:),[],'all');
 %     %     X = nanmean(X,1);
 %     XNorm = (X - mu)/sigma;
 %     %     X_conf_int = nan(2,length(operant_t));
@@ -38,18 +38,17 @@ end
 % on all other bats.
 target_source_batNums = {operant_listener_bat_num,operant_caller_bat_num};
 for target_source_k = 1:2
-    batIdx = ismember(mvgcStruct_comb.batNums,target_source_batNums{target_source_k});
-    FF_current = mvgcStruct_comb.FF_self(target_source_k,:,:,batIdx);
+%     batIdx = ismember(mvgcStruct_comb.batNums,target_source_batNums{target_source_k});
+    FF_current = mvgcStruct_comb.FF_self(target_source_k,:,:);
     mu = squeeze(nanmean(FF_current(:,mvgcStruct_comb.time<baselineT,:,:),'all'));
-    sigma = nanstd(FF_current(:,mvgcStruct_comb.time<baselineT,:,:),[],'all');
-    nBat = size(FF_current,4);
+    sigma = 1;% nanstd(FF_current(:,mvgcStruct_comb.time<baselineT,:,:),[],'all');
+    nBat = size(FF_current,ndims(FF_current));
     FFNorm = (FF_current - mu)/sigma;
     boundedline(mvgcStruct_comb.time,squeeze(nanmean(FFNorm,[3 4])),squeeze(nanstd(FFNorm,[],[3 4]))./sqrt(nBat),colors{target_source_k},'alpha')
 end
 h = findobj(gca,'Type','Line');
 legend(flipud(h),{'Operant Listener -> Caller','Operant Caller -> Listener','FC Listener -> Caller','FC Caller -> Listener'})
 xlim([-2.2 2.2])
-ylim([-1.25 3])
 axis square
 set(gca,'FontSize',25)
 legend box off
